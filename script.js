6b808c6fcf7dcc4fb2bfc838c3243d1ff9b6b594
Dennis Koeklue <dennis.koeklue@gmail.com>
2025-06-05 11:39:11 +0200

    added button for next Questions, color for the true and false Question and show current Number of the Questions (M:07 V: 08- 14

--- script.js
+++ script.js
@@ -69,7 +69,8 @@ function init() {
 
 function showQuestion() {
     let question = questions[currentQuestion];
-    
+
+    document.getElementById('currentQuestion').innerHTML = currentQuestion + 1;
     document.getElementById('question').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
@@ -77,4 +78,39 @@ function showQuestion() {
     document.getElementById('answer_4').innerHTML = question['answer_4'];
 
 
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`
+
+    if (selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success')
+    }else{
+        document.getElementById(selection).parentNode.classList.add('bg-danger')
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success')
+    }
+    
+  document.getElementById('next-button').disabled = false;
+}
+
+
+function nextQuestion() {
+    currentQuestion++;
+    currentNumberQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButton();
+    showQuestion();
+}
+
+function resetAnswerButton() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
